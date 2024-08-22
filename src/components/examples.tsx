6e195e76db7f3676428480d1d6a7677e4af10353
jooaogbriel <joao.ggwp@outlook.com>
2024-08-22 00:13:02 -0300

    refact: working with multiple jsx slots

--- src/components/examples.tsx
+++ src/components/examples.tsx
@@ -1,6 +1,7 @@
 import { SetStateAction, useState } from "react";
 import { EXAMPLES } from "../data";
 import { TabButton } from "./tabButton";
+import { Tabs } from "./tabs";
 
 export const Examples = () => {
   const [state, setState] = useState(null);
@@ -12,45 +13,51 @@ export const Examples = () => {
   return (
     <section id="examples">
       <h2>Examples</h2>
-      <menu>
-        <TabButton
-          isSelected={state === "components"}
-          onClick={() => handleClick("components")}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={state === "jsx"}
-          onClick={() => handleClick("jsx")}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={state === "props"}
-          onClick={() => handleClick("props")}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={state === "state"}
-          onClick={() => handleClick("state")}
-        >
-          State
-        </TabButton>
-      </menu>
-      <div id="tab-content">
-        {state === null ? (
-          <p>Please select a topic.</p>
-        ) : (
-          <div>
-            <h3>{EXAMPLES[state].title}</h3>
-            <p>{EXAMPLES.components.description}</p>
-            <pre>
-              <code>{EXAMPLES[state].code}</code>
-            </pre>
-          </div>
-        )}
-      </div>
+      <menu></menu>
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={state === "components"}
+              onClick={() => handleClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={state === "jsx"}
+              onClick={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={state === "props"}
+              onClick={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={state === "state"}
+              onClick={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        <div id="tab-content">
+          {state === null ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div>
+              <h3>{EXAMPLES[state].title}</h3>
+              <p>{EXAMPLES.components.description}</p>
+              <pre>
+                <code>{EXAMPLES[state].code}</code>
+              </pre>
+            </div>
+          )}
+        </div>
+      </Tabs>
     </section>
   );
 };
